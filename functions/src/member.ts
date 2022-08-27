126aed9344e61128c93390c22caa32966025265b
windy <windybirth@gmail.com>
2022-08-27 16:57:34 +0900

    add member

--- functions/src/member.ts
+++ functions/src/member.ts
@@ -214,6 +214,7 @@ export async function getMemberSteamIdAll():Promise<MemberInfo[]> {
   members.push(new MemberInfo(430821592, new Date("2022-09-25T00:00:00")));
   members.push(new MemberInfo(151516554, new Date("2022-09-26T00:00:00")));
   members.push(new MemberInfo(293124651, new Date("2022-09-26T00:00:00")));
+  members.push(new MemberInfo(330994098, new Date("2022-09-27T00:00:00")));
   // patreon
   members.push(new MemberInfo(67723423, new Date("2022-10-01T00:00:00")));
   members.push(new MemberInfo(1318433532, new Date("2022-10-01T00:00:00")));
@@ -398,6 +399,7 @@ export function initMemberSteamId():void {
   members.push(new Member(430821592, new Date("2022-09-25T00:00:00")));
   members.push(new Member(151516554, new Date("2022-09-26T00:00:00")));
   members.push(new Member(293124651, new Date("2022-09-26T00:00:00")));
+  members.push(new Member(330994098, new Date("2022-09-27T00:00:00")));
   // patreon
   members.push(new Member(67723423, new Date("2022-10-01T00:00:00")));
   members.push(new Member(1318433532, new Date("2022-10-01T00:00:00")));
