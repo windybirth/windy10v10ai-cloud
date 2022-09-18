4809645757a099ce6f209b7d963a32b1f3a776b8
windy <windybirth@gmail.com>
2022-09-19 02:53:06 +0900

    config firestore

--- cloud/src/app.module.ts
+++ cloud/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { getApp, initializeApp } from 'firebase-admin/app';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MembersModule } from './members/members.module';
@@ -8,4 +10,16 @@ import { MembersModule } from './members/members.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    // For debug and e2e test
+    // initializeApp({
+    //   storageBucket: 'windy10v10ai.appspot.com',
+    //   databaseURL: 'http://127.0.0.1:9000/?ns=windy10v10ai-default-rtdb',
+    //   projectId: 'windy10v10ai',
+    // });
+    // Initialize the firebase admin app
+    initializeApp();
+    console.log('[NestJs] firebase admin initialized.');
+  }
+}
