ef0bb3ebfe01a70ed51bc2fcabf0e5d4419b5fd9
windy <windybirth@gmail.com>
2022-10-06 23:54:09 +0900

    add open api swagger

--- cloud/src/main.ts
+++ cloud/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { AppGlobalSettings } from './util/settings';
@@ -6,6 +7,12 @@ import { AppGlobalSettings } from './util/settings';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   AppGlobalSettings(app);
+  const config = new DocumentBuilder()
+    .setTitle('Windy10v10 Cloud API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
   await app.listen(3000);
 }
 
