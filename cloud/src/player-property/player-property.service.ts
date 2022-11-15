e31d6a8fe4c989818f265607b8125be565b3eefc
windy <windybirth@gmail.com>
2022-11-16 01:39:28 +0900

    设计实装玩家属性储存 #62

--- cloud/src/player-property/player-property.service.ts
+++ cloud/src/player-property/player-property.service.ts
@@ -1,7 +1,6 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { BaseFirestoreRepository } from 'fireorm';
 import { InjectRepository } from 'nestjs-fireorm';
-import { map } from 'rxjs';
 
 import { PlayerService } from '../player/player.service';
 
@@ -13,9 +12,26 @@ import { PlayerProperty } from './entities/player-property.entity';
 export class PlayerPropertyService {
   static PROPERTY_NAME_LIST = [
     'property_cooldown_percentage',
-    'property_status_resistance_stacking',
     'property_cast_range_bonus_stacking',
+    'property_spell_amplify_percentage',
+    'property_status_resistance_stacking',
+    'property_magical_resistance_bonus',
+    'property_attack_range_bonus',
+    'property_physical_armor_bonus',
+    'property_preattack_bonus_damage',
+    'property_attackspeed_bonus_constant',
+    'property_stats_strength_bonus',
+    'property_stats_agility_bonus',
+    'property_stats_intellect_bonus',
+    'property_health_regen_percentage',
+    'property_mana_regen_total_percentage',
+    'property_lifesteal',
+    'property_spell_lifesteal',
+    'property_movespeed_bonus_constant',
+    'property_ignore_movespeed_limit',
+    'property_cannot_miss',
   ];
+
   constructor(
     @InjectRepository(PlayerProperty)
     private readonly playerPropertyRepository: BaseFirestoreRepository<PlayerProperty>,
