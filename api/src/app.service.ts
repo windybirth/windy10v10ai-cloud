613bbb5c91761b138c5e9eb0773c48de9a152711
windy <windybirth@gmail.com>
2024-02-27 00:59:18 +0900

    fix

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Hello, this is the root of API! Env: ${process.env.NODE_ENV}, ENVIRONMENT: ${process.env.ENVIRONMENT}, TEST_ENV: ${process.env.TEST_ENV}, TEST_ENV2: ${process.env.TEST_ENV2}`;
+    return `Hello, this is the root of API! Env: ${process.env.NODE_ENV}, ENVIRONMENT: ${process.env.ENVIRONMENT}`;
   }
 }
