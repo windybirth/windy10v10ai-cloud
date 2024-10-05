d1fb43468a8881f3674a88b5ac3194c6af033a54
windy <windybirth@gmail.com>
2024-10-05 10:47:27 +0900

    GA4_API_SECRET

--- api/src/util/secrets.ts
+++ api/src/util/secrets.ts
@@ -1,10 +1,15 @@
-export const SECRET = {
-  AFDIAN_TOKEN: 'AFDIAN_TOKEN',
-  PATREON_SECRET: 'PATREON_SECRET',
-  SERVER_APIKEY: 'SERVER_APIKEY',
-  SERVER_APIKEY_TEST: 'SERVER_APIKEY_TEST',
-};
+export enum SECRET {
+  AFDIAN_TOKEN = 'AFDIAN_TOKEN',
+  PATREON_SECRET = 'PATREON_SECRET',
+  SERVER_APIKEY = 'SERVER_APIKEY',
+  SERVER_APIKEY_TEST = 'SERVER_APIKEY_TEST',
+  GA4_API_SECRET = 'GA4_API_SECRET',
+}
 
-export const GetSecretValue = (key: string): string => {
-  return process.env[key];
+export const GetSecretValue = (key: SECRET): string => {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Secret value for ${key} is not defined`);
+  }
+  return value;
 };
